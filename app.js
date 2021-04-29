658ce17ed2bb30c596521bc280dbd861121b86e9
Pratham Rathour <prathamrathour2018@gmail.com>
2021-04-30 02:57:07 +0530

    29.4.21

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-hey there
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("hey there");
+});
+
+app.post('/',(req,response)=>{
+    res.send("posty");
+});
+const port =3000;
+
+app.listen(port,()=>{
+    console.log(`listenning on port ${port} `);
+})
