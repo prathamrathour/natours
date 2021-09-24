a1799640e0f1ff907b35573d2c46a09b93f944d3
Pratham Rathour <prathamrathour2018@gmail.com>
2021-09-24 16:34:27 +0530

    testing

--- app.js
+++ app.js
@@ -7,28 +7,27 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
+//testing
 // 1) MIDDLEWARES
-if(process.env.NODE_ENV ==='development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-  });
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // 2) ROUTES
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*',(req, res, next)=>{
-    next(new AppError(`cant find ${req.originalUrl} on this server`,404));
-
+app.all('*', (req, res, next) => {
+  next(new AppError(`cant find ${req.originalUrl} on this server`, 404));
 });
 
 app.use(globalErrorHandler);
